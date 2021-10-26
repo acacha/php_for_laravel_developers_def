f7e590836662c0f81e43e6053853e360099fff76
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-10-26 10:46:28 +0200

    Refactoritzar la vista tasks per utilitzar models Laravel (Laravel Eloquent)

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    // CRUD ->
+    // CREATE -> store
+    // RETRIEVE -> LIST i SHOW
+    // UPDATE -> update
+    // DELETE -> destroy
+}
--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,12 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 // HTTP GET
 Route::get('/', function () {
-    // POJO -> Plain Old Java Object
-    $task1 = new stdClass();
-    $task1->id = 1;
-    $task1->title = "Task 1";
-    $task1->description = "Bla bla bla";
-    $task1->completed = 1;
-    $task2 = new stdClass();
-    $task2->id = 2;
-    $task2->title = "Task 2";
-    $task2->description = "Bla bla bla";
-    $task2->completed = 1;
-
-    $task3 = new stdClass();
-    $task3->id = 3;
-    $task3->title = "Task 3";
-    $task3->description = "Bla bla bla";
-    $task3->completed = 0;
-
-    $tasks = [
-      $task1,
-      $task2,
-      $task3
-    ];
-
     return view('tasks',[
-        'tasks' => $tasks
+        'tasks' => Task::all()
     ]);
 });
 
